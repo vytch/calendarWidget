122b986fae3a6b663d0ab9cbb3bf13ce03f8c492
Vy Hoang <vy.hoang@revium.com.au>
2018-08-24 09:43:54 +1000

    Adding redux integration.

--- src/assets/js/modules/Event/Event.jsx
+++ src/assets/js/modules/Event/Event.jsx
@@ -3,21 +3,30 @@ import PropTypes from 'prop-types';
 import LandingPage from 'core-partials/display/templates/LandingPage';
 import DayView from 'core-partials/display/templates/DayView';
 import EventLayout from 'react-module/EventLayout';
+import {Provider} from 'react-redux';
 import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
+import appStore from './Stores/appStore';
 
 import './style.scss';
 
 /** This is the Event component. */
 class Event extends React.Component {
   render() {
+    const store = appStore({
+      reducer: {
+        testing: 'hello world',
+      },
+    });
     return (
       <div>
-        <Router history={hashHistory}>
-          <Route path="/" component={EventLayout}>
-            <IndexRoute component={LandingPage} />
-            <Route path="demo/:id" component={DayView} />
-          </Route>
-        </Router>
+        <Provider store={store}>
+          <Router history={hashHistory}>
+            <Route path="/" component={EventLayout}>
+              <IndexRoute component={LandingPage} />
+              <Route path="demo/:id" component={DayView} />
+            </Route>
+          </Router>
+        </Provider>
       </div>
     );
   }
--- src/assets/js/modules/EventLayout/EventLayout.jsx
+++ src/assets/js/modules/EventLayout/EventLayout.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import {Link } from 'react-router';
+import {updateTesting} from './Actions/TestingActions';
 import './style.scss';
 
 /** This is the EventLayout component. */
 class EventLayout extends React.Component {
+  constructor() {
+    super();
+    this._handleClick = this.clicked.bind(this);
+    console.log(updateTesting);
+  }
+  clicked(e) {
+    e.preventDefault();
+    this.props.dispatch(updateTesting('Blah blah'));
+  }
   render() {
     return (
-      <div>
+      <div className="event-layout">
         {this.props.children}
-        <ul>
+        <ul className="event-layout-navigation">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/demo/20180712">Demo 2018 07 13</Link></li>
           <li><Link to="/demo/20180713">Demo 2018 07 12</Link></li>
         </ul>
+        <h3>Reduc value:{this.props.testing}</h3>
+        <p>
+          <a href="#" onClick={this._handleClick}>Update me </a>
+        </p>
       </div>
     );
   }
@@ -25,6 +40,13 @@ EventLayout.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.string, PropTypes.element, PropTypes.array,
   ]).isRequired,
+  testing: PropTypes.string.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
-export default EventLayout;
+const mapStateToProps = state => {
+  return {
+    testing: state.reducer.testing,
+  };
+};
+export default connect(mapStateToProps)(EventLayout);
