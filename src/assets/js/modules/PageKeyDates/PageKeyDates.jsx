b428927aa056b3fc09c21b7cac7d178cd625d4f5
Jay Linsell <holla@jaylinsell.com>
2018-08-28 15:56:00 +1000

    Starting to filter out key dates

--- src/assets/js/modules/PageKeyDates/PageKeyDates.jsx
+++ src/assets/js/modules/PageKeyDates/PageKeyDates.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
+import EventBlock from 'core/Components/EventBlock';
+import keyDates from './keyDates.json';
 import './style.scss';
 
 /** This is the PageKeyDates component. */
 class PageKeyDates extends React.Component {
   render() {
+    const keyEvents = keyDates.filter(date => date.keyDate);
+
+    const days = keyDates.map(day => {
+      return (
+        <section key={`keyDate-${day.id}`} className="key-date">
+          <h2>{moment(day.start).format('MMMM DD YYYY')}</h2>
+
+          <EventBlock {...day} />
+        </section>
+      );
+    });
+
     return (
       <div className="key-dates">
         <header className="align-center">
-          <p>Date Block</p>
+          <p>Key Dates</p>
         </header>
+
+        { days }
       </div>
     );
   }
